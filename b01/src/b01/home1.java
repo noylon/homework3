9a9122c0a43c4b98cc74932174901319f066ef1c
ldh <noylon@naver.com>
2016-09-30 12:28:59 +0900

    과제1

---
+++ b01/src/b01/home1.java
@@ -0,0 +1,15 @@
+package b01;
+import java.util.Scanner;
+public class home1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		int num1, num2, num3;
+		int max_num, min_num;
+		
+		System.out.print("첫번재숫자를 입력하세요");
+		System.out.print("두번재숫자를 입력하세요");
+		System.out.print("세번재숫자를 입력하세요");
+	}
+}
